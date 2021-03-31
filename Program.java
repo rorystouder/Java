a7515fc0a9fef39e3f5320cef71ded4e870e4b98
rorystouder <rory.stouder@gmail.com>
2021-03-31 10:22:29 -0400

    A for loop to print buzz and fuzz every 3rd and 5th iteration.

---
+++ Program.java
@@ -0,0 +1,14 @@
+public class Program {
+  public static void main(String[] args){
+    for (int i = 0; i < 100; i++){
+      while (i++ <= 100) {
+        if ( i % 3 == 0 ){
+          System.out.println("buzz" + " " + i);
+        }
+        if (i % 5 == 0 ){
+          System.out.println("fuzz" + " " + i);
+      }
+     }
+    }
+  }
+}
